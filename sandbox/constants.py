fa4619dcaeea34a069d4a691208388bdd056230f
Ava Abderezaei <ava.abderezaei@gmail.com>
2024-09-18 12:38:13 -0600

    Clean generate gif script

--- sandbox/constants.py
+++ sandbox/constants.py
@@ -1,2 +1,5 @@
 LAYOUTS_IMAGES_DIR = 'data/layouts'
 QUESTIONNAIRES_DIR = 'data/questionnaires'
+
+SCREENSHOTS_DIR = 'data/screenshots'
+GIFS_DIR = 'data/gifs'
--- sandbox/generate_gif.py
+++ sandbox/generate_gif.py
@@ -1,19 +1,20 @@
 import os
 from PIL import Image
 
-# Path to the folder containing the images
-name = '3_chefs_small_kitchen_all_SP'
-image_folder = f'data/screenshots/{name}'
-output_gif = f'data/gifs/{name}.gif'
+from sandbox.constants import GIFS_DIR, SCREENSHOTS_DIR
 
-# List all image files and sort them numerically (assuming file names like 1.png, 2.png, etc.)
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-images.sort(key=lambda x: int(os.path.splitext(x)[0]))  # Sort numerically by the file name
 
-# Load images into a list
-frames = [Image.open(os.path.join(image_folder, image)) for image in images]
+def generate_gif(screenshots_folder_name, output_gif_name=None):
+    image_folder = f'{SCREENSHOTS_DIR}/{screenshots_folder_name}'
+    output_gif_name = output_gif_name if output_gif_name else screenshots_folder_name
+    output_gif = f'{GIFS_DIR}/{output_gif_name}.gif'
+    
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    images.sort(key=lambda x: int(os.path.splitext(x)[0]))
+    frames = [Image.open(os.path.join(image_folder, image)) for image in images]
+    frames[0].save(output_gif, save_all=True, append_images=frames[1:], optimize=False, duration=150, loop=0)
+    print(f"GIF created and saved as {output_gif}")
 
-# Save as a GIF
-frames[0].save(output_gif, save_all=True, append_images=frames[1:], optimize=False, duration=150, loop=0)
-
-print(f"GIF created and saved as {output_gif}")
+if __name__ == "__main__":
+    screenshots_folder_name = '3_chefs_forced_coordination'
+    generate_gif(screenshots_folder_name=screenshots_folder_name) # pass output_gif_name if you want to save the gif with a different name
