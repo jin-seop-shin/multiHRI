7257485b561cfc03835eeadbf48f5fbbd7ab1270
Ava Abderezaei <ava.abderezaei@gmail.com>
2024-10-07 15:41:38 -0600

    Add default for 3/5 player layouts

--- scripts/evaluate_agents.py
+++ scripts/evaluate_agents.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 mp.set_start_method('spawn', force=True) 
 
-import os
+import hashlib
 import sys
 from typing import Sequence
 import itertools
@@ -20,7 +20,19 @@ from oai_agents.agents.agent_utils import load_agent
 from oai_agents.common.arguments import get_arguments
 from oai_agents.gym_environments.base_overcooked_env import OvercookedGymEnv
 
-import hashlib
+from utils import (
+    TWO_PLAYERS_LOW_EVAL,
+    TWO_PLAYERS_MEDIUM_EVAL,
+    TWO_PLAYERS_HIGH_EVAL,
+    THREE_PLAYERS_LOW_EVAL,
+    THREE_PLAYERS_MEDIUM_EVAL,
+    THREE_PLAYERS_HIGH_EVAL,
+    FIVE_PLAYERS_LOW_EVAL,
+    FIVE_PLAYERS_MEDIUM_FOR_ALL_BESIDES_STORAGE_ROOM_EVAL,
+    FIVE_PLAYERS_HIGH_FOR_ALL_BESIDES_STORAGE_ROOM_EVAL,
+    FIVE_PLAYERS_MEDIUM_STORAGE_EVAL,
+    FIVE_PLAYERS_HIGH_STORAGE_EVAL
+)
 
 class Eval:
     LOW = 'l'
@@ -33,62 +45,53 @@ eval_key_lut = {
     'h': "HIGH"
 }
 
- # 0 - 100, 100-200, 200-300
-two_player_low = [
-    'agent_models/Result/Eval/2/SP_hd64_seed11/ck_0',
-    'agent_models/Result/Eval/2/SP_hd256_seed42/ck_1_rew_18.666666666666668',
-    'agent_models/Result/Eval/2/SP_hd64_seed11/ck_1_rew_22.0',
-    'agent_models/Result/Eval/2/SP_hd256_seed7/ck_1_rew_38.0',
-    'agent_models/Result/Eval/2/SP_hd64_seed1995/ck_0',
-]
-two_player_medium = [
-    'agent_models/Result/Eval/2/SP_hd64_seed11/ck_2_rew_108.66666666666667',
-    'agent_models/Result/Eval/2/SP_hd64_seed11/ck_3_rew_170.66666666666666',
-    'agent_models/Result/Eval/2/SP_hd256_seed7/ck_2_rew_106.66666666666667',
-    'agent_models/Result/Eval/2/SP_hd256_seed7/ck_3_rew_192.66666666666666',
-    'agent_models/Result/Eval/2/SP_hd256_seed42/ck_2_rew_110.66666666666667',
-]
-two_player_high = [
-    'agent_models/Result/Eval/2/SP_hd64_seed11/ck_24_rew_298.0',
-    'agent_models/Result/Eval/2/SP_hd64_seed1995/ck_20_rew_286.6666666666667',
-    'agent_models/Result/Eval/2/SP_hd256_seed7/ck_24_rew_258.0',
-    'agent_models/Result/Eval/2/SP_hd256_seed42/ck_22_rew_262.0',
-    'agent_models/Result/Eval/2/SP_hd256_seed42/ck_13_rew_274.0',
-]
-
-
 LAYOUT_NAMES_PATHs = {
     'selected_2_chefs_coordination_ring': {
-        Eval.LOW: two_player_low,
-        Eval.MEDIUM: two_player_medium,
-        Eval.HIGH:two_player_high
+        Eval.LOW: TWO_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: TWO_PLAYERS_MEDIUM_EVAL,
+        Eval.HIGH:TWO_PLAYERS_HIGH_EVAL
     },
     'selected_2_chefs_counter_circuit': {
-        Eval.LOW: two_player_low,
-        Eval.MEDIUM: two_player_medium,
-        Eval.HIGH:two_player_high
+        Eval.LOW: TWO_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: TWO_PLAYERS_MEDIUM_EVAL,
+        Eval.HIGH:TWO_PLAYERS_HIGH_EVAL
     },
     'selected_2_chefs_cramped_room': {
-        Eval.LOW: two_player_low,
-        Eval.MEDIUM: two_player_medium,
-        Eval.HIGH:two_player_high
+        Eval.LOW: TWO_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: TWO_PLAYERS_MEDIUM_EVAL,
+        Eval.HIGH:TWO_PLAYERS_HIGH_EVAL
     },
 
     'selected_3_chefs_coordination_ring': {
+        Eval.LOW: THREE_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: THREE_PLAYERS_MEDIUM_EVAL,
+        Eval.HIGH: THREE_PLAYERS_HIGH_EVAL,
     },
     'selected_3_chefs_counter_circuit': {
+        Eval.LOW: THREE_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: THREE_PLAYERS_MEDIUM_EVAL,
+        Eval.HIGH: THREE_PLAYERS_HIGH_EVAL,
     },
     'selected_3_chefs_cramped_room': {
-
+        Eval.LOW: THREE_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: THREE_PLAYERS_MEDIUM_EVAL,
+        Eval.HIGH: THREE_PLAYERS_HIGH_EVAL,
     },
-    'selected_5_chefs_counter_circuit': {
 
+    'selected_5_chefs_counter_circuit': {
+        Eval.LOW: FIVE_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: FIVE_PLAYERS_MEDIUM_FOR_ALL_BESIDES_STORAGE_ROOM_EVAL,
+        Eval.HIGH: FIVE_PLAYERS_HIGH_FOR_ALL_BESIDES_STORAGE_ROOM_EVAL,
     },
     'selected_5_chefs_secret_coordination_ring': {
-
+        Eval.LOW: FIVE_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: FIVE_PLAYERS_MEDIUM_FOR_ALL_BESIDES_STORAGE_ROOM_EVAL,
+        Eval.HIGH: FIVE_PLAYERS_HIGH_FOR_ALL_BESIDES_STORAGE_ROOM_EVAL,
     },
     'selected_5_chefs_storage_room': {
-
+        Eval.LOW: FIVE_PLAYERS_LOW_EVAL,
+        Eval.MEDIUM: FIVE_PLAYERS_MEDIUM_STORAGE_EVAL,
+        Eval.HIGH: FIVE_PLAYERS_HIGH_STORAGE_EVAL,
     },
 }
 
@@ -299,7 +302,7 @@ def evaluate_agent_for_layout(agent_name, path, layout_names, p_idxes, args, det
 
 def run_parallel_evaluation(args, all_agents_paths, layout_names, p_idxes, deterministic, max_num_teams_per_layout_per_x, number_of_eps, teammate_lvl_sets: Sequence[Sequence[Eval]]):
     all_mean_rewards, all_std_rewards = {}, {}
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
         futures = [
             executor.submit(evaluate_agent_for_layout, name, path, layout_names, p_idxes, args, deterministic, max_num_teams_per_layout_per_x, number_of_eps, teammate_lvl_set)
             for (name, path), teammate_lvl_set in itertools.product(all_agents_paths.items(), teammate_lvl_sets)
@@ -316,61 +319,6 @@ def run_parallel_evaluation(args, all_agents_paths, layout_names, p_idxes, deter
 
     return all_mean_rewards, all_std_rewards
 
-
-# def get_3_player_input(args):
-#     args.num_players = 3
-#     layout_names = ['3_chefs_small_kitchen_two_resources',
-#                     '3_chefs_forced_coordination_3OP2S1D',
-#                     '3_chefs_asymmetric_advantages',
-#                     '3_chefs_counter_circuit'
-#                     ]
-#     p_idxes = [0,1,2]
-
-#     # the name on the left is shown on the plot
-#     all_agents_paths = {
-#         'N-1-SP_cur': 'agent_models/sp-vs-spwsp/3-chefs-all-layouts/spWsp_s1010_h256_tr(SPH_SPM_SPL)_cur/best',
-#         'N-1-SP_ran': 'agent_models/sp-vs-spwsp/3-chefs-all-layouts/spWsp_s1010_h256_tr(SPH_SPM_SPL)_cur/best',
-#         #'Saboteur Play': 'agent_models/four-layouts/supporter-fishplay/024_14/pwsab_s68_h512_tr(SP_SPH)_ran/best',
-#         #'FCP': 'agent_models/all_layouts_supporters/fcp_s2020_h256_tr(AMX)_ran/best',
-#         #'SP': 'agent_models/N-1-SP-vs-AP-supporter-howlong-4/SP_hd64_seed14/ck_40_rew_316.0'
-#     }
-#     teammate_lvl_sets = [
-#         [Eval.LOW],
-#     ]
-#     return layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args
-
-
-# def get_5_player_input(args):
-#     args.num_players = 5
-#     layout_names = ['5_chefs_storage_room_lots_resources',
-#                     '5_chefs_clustered_kitchen',
-#                     '5_chefs_coordination_ring'
-#                     ]
-#     p_idxes = [0, 1, 2, 3, 4]
-#     all_agents_paths = {
-#         'FCP': 'agent_models/5-p-layouts/FCP_s2020_h256_tr(AMX)_ran/best',
-#         'SP': 'agent_models/5-p-layouts/SP_hd256_seed13/ck_20_rew_441.3333333333333',
-#         'N-1-SP ran': 'agent_models/N-X-SP/N-1-SP_s1010_h256_tr(SPH_SPM_SPL)_ran/best',
-#         'N-1-SP cur': 'agent_models/N-4-SP/N-1-SP_s1010_h256_tr(SPH_SPM_SPL)_cur/best',
-#         'N-3-SP ran': 'agent_models/N-3-SP/N-3-SP_s1010_h256_tr(SPH_SPM_SPL)_ran/best',
-#         'N-3-SP cur': 'agent_models/N-3-SP/N-3-SP_s1010_h256_tr(SPH_SPM_SPL)_cur/best',
-#         'N-4-SP ran': 'agent_models/N-4-SP/N-4-SP_s1010_h256_tr(SPH_SPM_SPL)_ran/best',
-#         'N-4-SP cur': 'agent_models/N-4-SP/N-4-SP_s1010_h256_tr(SPH_SPM_SPL)_cur/best'
-#     }
-#     return layout_names, p_idxes, all_agents_paths, args
-
-# def get_new_5_player_input(args):
-#     args.num_players = 5
-#     layout_names = ['5_chefs_storage_room_lots_resources', '5_chefs_clustered_kitchen', '5_chefs_coordination_ring']
-#     p_idxes = [0, 1, 2, 3 ,4]
-#     all_agents_paths = {
-#         'N-1-SP cur': 'agent_models/N-1-SP-4-long/N-1-SP_s1010_h256_tr(SPH_SPM_SPL)_cur/best',
-#         'N-1-SP ran': 'agent_models/N-1-SP-4-long/N-1-SP_s1010_h256_tr(SPH_SPM_SPL)_ran/best',
-#         'SP': 'agent_models/N-1-SP-4-long/SP_hd256_seed68/best',
-#         'FCP': 'agent_models/N-1-SP-4-long/FCP_s2020_h256_tr(AMX)_ran/best',
-#         'Adversary Play': 'agent_models/M2FP-1/sp_s68_h512_tr(SP)_ran/M2FP/supporter-selfisherplay/0/pwadv_s68_h512_tr(SP_SPADV)_ran/best'
-#     }
-#     return layout_names, p_idxes, all_agents_paths, args
 
 
 def get_2_player_input(args):
@@ -395,9 +343,48 @@ def get_2_player_input(args):
     return layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args
 
 
+def get_3_player_input(args):
+    args.num_players = 3
+    layout_names = ['selected_3_chefs_coordination_ring',
+                    'selected_3_chefs_counter_circuit',
+                    'selected_3_chefs_cramped_room']
+    p_idxes = [0, 1, 2]
+    all_agents_paths = {
+        'N-1-SP FCP CUR':  'agent_models/Result/3/N-1-SP_s1010_h256_tr(SPH_SPH_SPH_SPH_SPM_SPM_SPM_SPM_SPL_SPL_SPL_SPL)_cur/best',
+        'N-1-SP FCP RAN':  'agent_models/Result/3/N-1-SP_s1010_h256_tr(SPH_SPH_SPH_SPH_SPM_SPM_SPM_SPM_SPL_SPL_SPL_SPL)_ran/best',
+        'SP':              'agent_models/Result/3/SP_hd64_seed14/best',
+        'FCP':             'agent_models/Result/3/FCP_s2020_h256_tr(AMX)_ran/best'}
+    teammate_lvl_sets = [
+        [Eval.LOW],
+        [Eval.MEDIUM],
+        [Eval.HIGH]
+    ]
+    return layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args
+
+
+def get_five_player_input(args):
+    args.num_players = 5
+    layout_names = ['selected_5_chefs_counter_circuit',
+                    'selected_5_chefs_secret_coordination_ring',
+                    'selected_5_chefs_storage_room']
+    p_idxes = [0, 1, 2, 3, 4]
+    all_agents_paths = {
+        'N-1-SP FCP CUR':  'agent_models/Result/5/N-1-SP_s1010_h256_tr(SPH_SPH_SPH_SPH_SPM_SPM_SPM_SPM_SPL_SPL_SPL_SPL)_cur/best',
+        'N-1-SP FCP RAN':  'agent_models/Result/5/N-1-SP_s1010_h256_tr(SPH_SPH_SPH_SPH_SPM_SPM_SPM_SPM_SPL_SPL_SPL_SPL)_ran/best',
+        'SP':              'agent_models/Result/5/SP_hd64_seed14/best',
+        'FCP':             'agent_models/Result/5/FCP_s2020_h256_tr(AMX)_ran/best'}
+    teammate_lvl_sets = [
+        [Eval.LOW],
+        [Eval.MEDIUM],
+        [Eval.HIGH]
+    ]
+    return layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args
+
+
 if __name__ == "__main__":
     args = get_arguments()
-    layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args = get_2_player_input(args)
+    # layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args = get_2_player_input(args)
+    layout_names, p_idxes, all_agents_paths, teammate_lvl_sets, args = get_3_player_input(args)
     
     deterministic = False
     max_num_teams_per_layout_per_x = 4
