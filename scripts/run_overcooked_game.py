1cba7371fef5d738ccb8ae5b1072e56b126b457a
ttopiac <ttopiac@gmail.com>
2025-03-15 19:10:55 -0600

    Make the run_overcooked_game.py script the same as the previous one

--- scripts/run_overcooked_game.py
+++ scripts/run_overcooked_game.py
@@ -15,7 +15,7 @@ def get_teammate_from_pop_file(tm_name, tm_score, pop_path, layout_name):
 
 if __name__ == "__main__":
     args = get_arguments()
-    args.num_players = 1
+    args.num_players = 5
 
     'dec_5_chefs_counter_circuit',
     'dec_5_chefs_storage_room',
@@ -23,27 +23,12 @@ if __name__ == "__main__":
     'selected_5_chefs_spacious_room_no_counter_space',
 
 
-    # args.layout = f'dec_5_chefs_counter_circuit'
+    args.layout = f'dec_5_chefs_counter_circuit'
     args.p_idx = 0
+    args.layout_names = [args.layout]
     args.n_envs = 1
 
-    if args.num_players == 1:
-        # args.layout = 'storage_room_single_right_sym_leftpotblocked'
-        # args.layout = 'simple_o_t_single'
-        args.layout = 'cramped_room_single_ot'
-        args.layout_names = [args.layout]
-        # player_path = 'agent_models/storage_room_1_chef_layouts/1/SP_s13_h256_tr[SP]_ran/best'
-        # player = load_agent(Path(player_path), args)
-        teammates = []
-    elif args.num_players == 2:
-        args.layout = 'storage_room_pair_left_sym_rightpotblocked'
-        args.layout_names = [args.layout]
-        player_path = 'agent_models/Complex/2/SP_s1010_h256_tr[SP]_ran/best'
-        player = load_agent(Path(player_path), args)
-        teammates = [player]
-
     # teammates_path = [
-    #     'agent_models/Classic/2/SP_s1010_h256_tr[SP]_ran/ck_0'
     #     'agent_models/ALMH_CUR/2/SP_hd64_seed14/best', # green
     #     'agent_models/ALMH_CUR/2/SP_hd64_seed14/best', # orange
     #     'agent_models/ALMH_CUR/2/SP_hd64_seed14/best',
@@ -55,15 +40,12 @@ if __name__ == "__main__":
 
     # trajectories = tile locations. Top left of the layout is (0, 0), bottom right is (M, N)
     # teammates = [CustomAgent(args=args, name='human', trajectories={args.layout: [(8, 1), (8, 2), (7, 2), (6, 2)]})]
-    # teammates = [DummyAgent(action='random') for _ in range(args.num_players - 1)]
+    teammates = [DummyAgent(action='random') for _ in range(args.num_players - 1)]
 
-    # player_path = 'agent_models/storage_room_1_chef_layouts/1/random_positions/SP_s2602_h256_tr[SP]_ran/best'
+    # player_path = 'agent_models/ALMH_CUR/2/SP_hd64_seed14/best'
     # player = load_agent(Path(player_path), args)
-    # teammates = []
     player = 'human' # blue
-    # player = DummyAgent(action='random')
 
     dc = OvercookedGUI(args, agent=player, teammates=teammates, layout_name=args.layout, p_idx=args.p_idx, fps=10,
                         horizon=400, gif_name=args.layout)
     dc.on_execute()
-
